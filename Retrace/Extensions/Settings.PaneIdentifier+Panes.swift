4afad0d6bd6dfc4446f421bf84be503ebe808a8c
L.L <dj7946@qq.com>
2026-06-17 02:27:09 +0800

    Remove unused settings pane identifier

--- Retrace/Extensions/Settings.PaneIdentifier+Panes.swift
+++ Retrace/Extensions/Settings.PaneIdentifier+Panes.swift
@@ -1,7 +1,6 @@
 import Settings
 
 extension Settings.PaneIdentifier {
-  static let advanced = Self("advanced")
   static let appearance = Self("appearance")
   static let general = Self("general")
 }
