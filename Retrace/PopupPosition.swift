668ebb173f71821efa8ae2115cb15b4a371d505a
L.L <dj7946@qq.com>
2026-06-17 03:52:59 +0800

    Split popup position origins

--- Retrace/PopupPosition.swift
+++ Retrace/PopupPosition.swift
@@ -26,42 +26,59 @@ enum PopupPosition: String, CaseIterable, Identifiable, CustomStringConvertible,
     }
   }
 
-  // swiftlint:disable:next cyclomatic_complexity
   func origin(size: NSSize, statusBarButton: NSStatusBarButton?) -> NSPoint {
     switch self {
     case .center:
-      if let frame = NSScreen.forPopup?.visibleFrame {
-        return NSRect.centered(ofSize: size, in: frame).origin
-      }
+      return screenCenterOrigin(size: size) ?? cursorOrigin(size: size)
     case .window:
-      if let frame = NSWorkspace.shared.frontmostApplication?.windowFrame {
-        return NSRect.centered(ofSize: size, in: frame).origin
-      }
+      return windowCenterOrigin(size: size) ?? cursorOrigin(size: size)
     case .statusItem:
-      if let statusBarButton, let screen = NSScreen.main {
-        let rectInWindow = statusBarButton.convert(statusBarButton.bounds, to: nil)
-        if let screenRect = statusBarButton.window?.convertToScreen(rectInWindow) {
-          var topLeftPoint = NSPoint(x: screenRect.minX, y: screenRect.minY - size.height)
-          // Ensure that window doesn't spill over to the right screen.
-          if (topLeftPoint.x + size.width) > screen.frame.maxX {
-            topLeftPoint.x = screen.frame.maxX - size.width
-          }
-
-          return topLeftPoint
-        }
-      }
+      return statusItemOrigin(size: size, statusBarButton: statusBarButton) ?? cursorOrigin(size: size)
     case .lastPosition:
-      if let frame = NSScreen.forPopup?.visibleFrame {
-        let relativePos = Defaults[.windowPosition]
-        let anchorX = frame.minX + frame.width * relativePos.x
-        let anchorY = frame.minY + frame.height * relativePos.y
-        // Anchor is top middle of frame
-        return NSPoint(x: anchorX - size.width / 2, y: anchorY - size.height)
-      }
+      return lastPositionOrigin(size: size) ?? cursorOrigin(size: size)
     default:
-      break
+      return cursorOrigin(size: size)
+    }
+  }
+
+  private func screenCenterOrigin(size: NSSize) -> NSPoint? {
+    guard let frame = NSScreen.forPopup?.visibleFrame else { return nil }
+
+    return NSRect.centered(ofSize: size, in: frame).origin
+  }
+
+  private func windowCenterOrigin(size: NSSize) -> NSPoint? {
+    guard let frame = NSWorkspace.shared.frontmostApplication?.windowFrame else { return nil }
+
+    return NSRect.centered(ofSize: size, in: frame).origin
+  }
+
+  private func statusItemOrigin(size: NSSize, statusBarButton: NSStatusBarButton?) -> NSPoint? {
+    guard let statusBarButton, let screen = NSScreen.main else { return nil }
+
+    let rectInWindow = statusBarButton.convert(statusBarButton.bounds, to: nil)
+    guard let screenRect = statusBarButton.window?.convertToScreen(rectInWindow) else { return nil }
+
+    var topLeftPoint = NSPoint(x: screenRect.minX, y: screenRect.minY - size.height)
+    // Ensure that window doesn't spill over to the right screen.
+    if (topLeftPoint.x + size.width) > screen.frame.maxX {
+      topLeftPoint.x = screen.frame.maxX - size.width
     }
 
+    return topLeftPoint
+  }
+
+  private func lastPositionOrigin(size: NSSize) -> NSPoint? {
+    guard let frame = NSScreen.forPopup?.visibleFrame else { return nil }
+
+    let relativePos = Defaults[.windowPosition]
+    let anchorX = frame.minX + frame.width * relativePos.x
+    let anchorY = frame.minY + frame.height * relativePos.y
+    // Anchor is top middle of frame.
+    return NSPoint(x: anchorX - size.width / 2, y: anchorY - size.height)
+  }
+
+  private func cursorOrigin(size: NSSize) -> NSPoint {
     var point = NSEvent.mouseLocation
     point.y -= size.height
     return point
